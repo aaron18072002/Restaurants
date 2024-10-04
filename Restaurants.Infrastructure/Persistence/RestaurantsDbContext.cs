1fb923e6e822be458fe670932b8bb94f4cdda165
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-04 10:50:09 +0700

    Install and config ef sql server

---
+++ Restaurants.Infrastructure/Persistence/RestaurantsDbContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Restaurants.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurants.Infrastructure.Persistence
+{
+    public class RestaurantsDbContext : DbContext
+    {
+        public DbSet<Restaurant> Restaurants { get; set; }
+        public DbSet<Dish> Dishes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=THANHANH\SQLEXPRESS;Database=RestaurantsDb;Integrated Security=True;TrustServerCertificate=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Restaurant>()
+                .OwnsOne(r => r.Address);
+
+            modelBuilder.Entity<Restaurant>()
+                .HasMany(r => r.Dishes)
+                .WithOne()
+                .HasForeignKey(d => d.RestaurantId);
+        }
+    }
+}

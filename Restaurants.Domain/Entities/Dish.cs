88212f36ecc73b8a40c2d30c3866d4f801ab15cd
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-04 10:49:51 +0700

    Add annotations to config type of fields and create foreign key

--- Restaurants.Domain/Entities/Dish.cs
+++ Restaurants.Domain/Entities/Dish.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,21 @@ namespace Restaurants.Domain.Entities
 {
     public class Dish
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
         public string Description { get; set; } = string.Empty;
+
+        [Required]
         public decimal Price { get; set; }
+
+        //[ForeignKey("RestaurantId")]
+        public int RestaurantId { get; set; }
     }
 }
--- Restaurants.Domain/Entities/Restaurant.cs
+++ Restaurants.Domain/Entities/Restaurant.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,13 +9,27 @@ namespace Restaurants.Domain.Entities
 {
     public class Restaurant
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(40)]
         public string Name { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(100)]
         public string Description { get; set; } = string.Empty;
+
+        [Required]
+        [MaxLength(40)]
         public string Category {  get; set; } = string.Empty;
         public bool HasDelivery { get; set; }
 
+        [MaxLength(40)]
         public string? ContactEmail { get; set; }
+
+        [Required]
+        [MaxLength(10)]
         public string? ContactNumber { get; set; }
 
         public Address? Address { get; set; }

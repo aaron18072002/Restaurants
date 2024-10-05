851ad0851af56a842ed8e9205eabb27a5f49eb5f
aaron18072002 <nguyenthanhanhtp123@gmail.com>
2024-10-05 14:41:00 +0700

    Create create restaurant controller and inject service

---
+++ Restaurants.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Restaurants.Application.Restaurants;
+
+namespace Restaurants.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RestaurantsController : ControllerBase
+    {
+        private readonly IRestaurantsService _restaurantsService;
+        private readonly ILogger<RestaurantsController> _logger;
+        public RestaurantsController(IRestaurantsService restaurantsService, ILogger<RestaurantsController> logger)
+        {
+            this._restaurantsService = restaurantsService;
+            this._logger = logger;
+        }
+
+        [HttpGet]
+        [Route("")]
+        [Route("[action]")]
+        public async Task<IActionResult> GetAll()
+        {
+            var restaurant = await this._restaurantsService.GetAllRestaurants();
+
+            return this.Ok(restaurant);
+        }
+    }
+}
--- Restaurants.API/Program.cs
+++ Restaurants.API/Program.cs
@@ -1,4 +1,5 @@
 using Restaurants.Infrastructure.Extensions;
+using Restaurants.Application.Extensions;
 using Restaurants.Infrastructure.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddServices();
 
 var app = builder.Build();
 
